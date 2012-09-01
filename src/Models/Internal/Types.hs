9f662138709e2d107418acdc0f8b3a61513845c7
Haisheng.W.WU <freizl@gmail.com>
2012-09-01 09:46:41 +0800

    rename Persistent api

--- src/Models/Internal/Types.hs
+++ src/Models/Internal/Types.hs
@@ -5,45 +5,48 @@
 
 module Models.Internal.Types where
 
-import Snap.Core
+import           Snap.Core
+import           Snap
 import           Database.MongoDB
 import qualified Database.MongoDB as DB
 import           Snap.Snaplet.Auth
 import           Snap.Snaplet.MongoDB
-import Control.Monad.Trans
+import           Control.Monad.Trans
 
-import           Application
 import           Models.Exception
-import           Models.Utils
 
 -- | A simple generic Persistent handler.
 -- 
 -- 
-class Persistent a where
+class MongoDBPersistent a where
   -- | Schema name
   --   Implement me like `getSchemaP _ = u "abc"`
-  getSchemaP :: a -> Collection
+  mongoColl :: a -> Collection
   
   -- | Transform Data to MongoDB document type
-  toMongoDocP :: a -> Document
+  toMongoDoc :: a -> Document
   
   -- | Transform MongoDB document to perticular type
-  fromMongoDocP :: Document -> IO a
+  fromMongoDoc :: Document -> IO a
   
   -- | FIXME: Insert ID after save successfully. @see Topic.hs: 51
   -- | Simple MongoDB Save Operation 
   -- 
-  saveP :: a -> AppHandler a
-  saveP x = do
-            res <- eitherWithDB $ DB.save (getSchemaP x) (toMongoDocP x)
+  mongoInsert :: (MonadIO m, MonadState app m, HasMongoDB app) 
+           => a    -- ^ new model that will be save
+           -> m a  -- ^ saved model with id.
+  mongoInsert x = do
+            res <- eitherWithDB $ DB.insert (mongoColl x) (toMongoDoc x)
             either failureToUE (const $ return x) res
   
   -- | Fetch All items in the collection
   -- 
-  getAllP :: a -> AppHandler [a]
-  getAllP x  = do
-               let selection = select [] (getSchemaP x)
+  mongoFindAll :: (MonadIO m, MonadState app m, HasMongoDB app) 
+             => a               -- ^ an empty model. (work around for the concern below.
+             -> m [a]  -- ^ list of model data that has been retrieved.
+  mongoFindAll x  = do
+               let selection = select [] (mongoColl x)
                -- let selection = select [] (getSchemaP (undefined::a)) WHY IT FAILED.?             
                xs <- eitherWithDB $ rest =<< find (selection)
-               liftIO $ mapM fromMongoDocP $ either (const []) id xs
+               liftIO $ mapM fromMongoDoc $ either (const []) id xs
 
--- src/Models/Tag.hs
+++ src/Models/Tag.hs
@@ -23,39 +23,56 @@ import           Models.Internal.Types
 
 --import           Control.Monad.Trans
 
--- | 
+-- | Tag model
 -- 
 data Tag = Tag
-    { _tagId :: Maybe ObjectId
-    , _tagName   :: T.Text
+    { _tagId      :: Maybe ObjectId
+    , _tagName    :: T.Text
     , _tagContent :: T.Text
     } deriving (Show, Eq)
 
+-- | Schema name
+-- 
 tagCollection :: Collection
 tagCollection = u "tags"
 
-------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
+-- Impl of Persistent Interface
+--------------------------------------------------------------------------------
 
-instance Persistent Tag where
-  getSchemaP _ = tagCollection
-  toMongoDocP = tagToDocument
-  fromMongoDocP = tagFromDocumentOrThrow
-  
-------------------------------------------------------------------------------
+
+-- | FIXME: couple of thoughts: 
+--   0. save or create ?
+--   1. Input from web is [String]
+--   2. batch create new tags?
+--   3. insert [ObjectId] to topic tabel
+-- 
+instance MongoDBPersistent Tag where
+  mongoColl _  = tagCollection
+  toMongoDoc   = tagToDocument
+  fromMongoDoc = tagFromDocumentOrThrow
+
+
+--------------------------------------------------------------------------------
+-- CRUD
+--------------------------------------------------------------------------------
+
 
 -- | save a new tag.
 --   meaning insert it if its new (has no "_id" field) or update it if its not new (has "_id" field)
 --   TODO: better to make sure _id exists because Nothing objectId will cause error other when viewing.
-saveTag :: Tag -> AppHandler Tag
-saveTag = saveP
+insertTag :: Tag -> AppHandler Tag
+insertTag = mongoInsert
 
------------------------------------------
 
 findAllTags :: AppHandler [Tag]
-findAllTags  = getAllP undefined
+findAllTags  = mongoFindAll undefined
 
------------------------------------------
+
+--------------------------------------------------------------------------------
 -- MongoDB Document Transform
+--------------------------------------------------------------------------------
+
 
 -- | Transform @Tag@ to mongoDB document.
 --   Nothing of id mean new topic thus empty "_id" let mongoDB generate objectId.
