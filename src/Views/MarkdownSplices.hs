23834c8ce718e207a329fb69b82db7bda5492efe
Haisheng.W.WU <freizl@gmail.com>
2012-10-20 02:17:01 +0800

    XSS

--- src/Views/MarkdownSplices.hs
+++ src/Views/MarkdownSplices.hs
@@ -8,16 +8,14 @@ module Views.MarkdownSplices
        ( markdownToHtmlSplice ) where
 
 import qualified Codec.Binary.UTF8.String               as UTF8
-import           Control.Monad.CatchIO
 import           Control.Monad.Trans
 import qualified Data.ByteString.Char8                  as BS
 import qualified Data.Text                              as T
 import           Text.Pandoc
 import           Text.Pandoc.Shared                     (tabFilter)
 import           Text.Templating.Heist
-import           Text.Templating.Heist.Splices.Markdown
 import qualified Text.XmlHtml                           as X
-
+import           Text.HTML.SanitizeXSS (sanitizeBalance)
 
 ----------------------------------------------------------------------
 
@@ -34,7 +32,7 @@ tabFilter4 :: String -> String
 tabFilter4 = tabFilter 4
 
 markdownToHtmlString :: T.Text -> BS.ByteString
-markdownToHtmlString = BS.pack . writeDoc . readDoc . tabFilter4 . T.unpack
+markdownToHtmlString = BS.pack . writeDoc . readDoc . tabFilter4 . T.unpack . sanitizeBalance
 
 readDoc :: String -> Pandoc
 readDoc = readMarkdown (defaultParserState { stateLiterateHaskell = True })
