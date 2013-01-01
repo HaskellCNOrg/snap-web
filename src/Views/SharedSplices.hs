880a07b3a3ce3cfc3ff20fc407ec79c78e6e29c9
Haisheng.W.WU <freizl@gmail.com>
2012-12-31 23:18:51 -0500

    deprecated showOnEnv function

--- src/Views/SharedSplices.hs
+++ src/Views/SharedSplices.hs
@@ -16,27 +16,11 @@ import qualified Text.XmlHtml              as X
 
 
 ----------------------------------------------------------------------------
-{-
-currentEnv :: T.Text
-#ifdef DEVELOPMENT
-currentEnv = "devel"
-#else
-currentEnv = "prod"
-#endif
--}
-
--- | DEPRECATED a seprated layout-css.tpl is created and do replacement at prod build.
---
-currentEnv :: AppHandler T.Text
-currentEnv = lookupConfigDefault "env" "devel"
-
-----------------------------------------------------------------------------
 
 
 sharedSplices :: [(T.Text, SnapletSplice App App)]
 sharedSplices = [ ("currentUser", currentUserSplice)
                 , ("isCurrentUserAdmin", isCurrentUserAdminSplice)
-                , ("showOnEnv", liftHeist showOnEnvSplice)
                 ]
 
 
@@ -53,13 +37,3 @@ isCurrentUserAdminSplice :: SnapletSplice App App
 isCurrentUserAdminSplice = do
     tf <- liftHandler isCurrentUserAdmin
     if tf then liftHeist runChildren else return []
-
--- | Show children per Production or Development
---   e.g. <showOnEnv on="devel">.../</showOnEnv>
---
-showOnEnvSplice :: Splice AppHandler
-showOnEnvSplice = do
-    node <- getParamNode
-    v <- lift currentEnv
-    return (if getOnAttr node == v then X.elementChildren node else [])
-    where getOnAttr n = fromMaybe "" (X.getAttribute "on" n)
